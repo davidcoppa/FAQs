2cec86510f0801743fbcb6367015698f1dca93a6
davidcoppa <davidcoppar@gmail.com>
2022-06-27 11:24:32 +1200

    Initial commit

---
+++ FAQs/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FAQs.Models;
+
+namespace FAQs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FAQs.Models.FAQs>? FAQs { get; set; }
+    }
+}
---
+++ FAQs/Models/FAQs.cs
@@ -0,0 +1,14 @@
+﻿namespace FAQs.Models
+{
+    public class FAQs
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public FAQs()
+        {
+
+        }
+    }
+}
